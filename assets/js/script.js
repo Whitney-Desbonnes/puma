11a83372d42b225f51833b71d4d88b9ef41665f9
Whitney Desbonnes <whitney.desbonnes@gmail.com>
2022-10-12 23:59:55 +0200

    responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,26 @@
 let video = document.getElementById('video');
 let sectionVideo = document.getElementById('pumaVideo');
 let sectionAdresse = document.getElementById('pumaAdresse');
-let arrowLink = document.getElementById('link');
-console.log(video);
+let arrow = document.getElementById('arrow');
 
-
-video.addEventListener('ended', masquer, false);
-arrowLink.addEventListener('click', jump);
-
+// Fonction pour masquer la vidéo une fois la lecture terminer
 function masquer () {
-    sectionVideo.classList.add('hide');
+    sectionVideo.classList.add('hide'); // ajoute la class hide à la section video poru la faire disparaitre
+
     setTimeout(() => {
         sectionAdresse.classList.add('show');
-    },1000);
+    },1000); // fait apparaitre la section adresse en ajoutant la classe show, après 1s
+
+    setTimeout(() => {
+        arrow.classList.add('none');
+    },2000); // fait disparaitre la fleche en ajoutant la classe none, après 2s
 }
 
+// Au click sur la flème met pause à la vidéo et lance la fonction masquer()
 function jump () {
-    video.pause()
-    masquer();
+    video.pause(); //pause
+    masquer(); // lance la fonction masquer()
 }
+
+video.addEventListener('ended', masquer, false); // ecouteur de fin lecture video
+arrow.addEventListener('click', jump); // ecouteur du click sur la fleche
