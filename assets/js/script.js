bcd79b64a05637127fdaf929111d857935205708
Whitney Desbonnes <whitney.desbonnes@gmail.com>
2022-10-13 23:40:41 +0200

    dernier ajustements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// déclaration des variables
 let video = document.getElementById('video');
 let sectionVideo = document.getElementById('pumaVideo');
 let sectionAdresse = document.getElementById('pumaAdresse');
@@ -5,22 +6,22 @@ let arrow = document.getElementById('arrow');
 
 // Fonction pour masquer la vidéo une fois la lecture terminer
 function masquer () {
-    sectionVideo.classList.add('hide'); // ajoute la class hide à la section video poru la faire disparaitre
+    sectionVideo.classList.add('hide'); // ajoute la classe "hide" à la section video pour la faire disparaitre
 
     setTimeout(() => {
         sectionAdresse.classList.add('show');
-    },1000); // fait apparaitre la section adresse en ajoutant la classe show, après 1s
+    },1000); // ajoute la classe "show" pour faire apparaitre la section adresse, après un délai de 1s
 
     setTimeout(() => {
         arrow.classList.add('none');
-    },2000); // fait disparaitre la fleche en ajoutant la classe none, après 2s
+    },2000); // ajoute la classe "none" pour faire disparaitre la flèche, après un délai de 2s
 }
 
-// Au click sur la flème met pause à la vidéo et lance la fonction masquer()
+// Au click sur la flèche met pause à la vidéo et lance la fonction masquer()
 function jump () {
     video.pause(); //pause
     masquer(); // lance la fonction masquer()
 }
 
-video.addEventListener('ended', masquer, false); // ecouteur de fin lecture video
-arrow.addEventListener('click', jump); // ecouteur du click sur la fleche
+video.addEventListener('ended', masquer); // écouteur de fin lecture video
+arrow.addEventListener('click', jump); // écouteur du click sur la fleche
